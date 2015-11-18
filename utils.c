de512547e840d9e27a2667ef3f3b30112ced7b01
Yinlong Su <suyinlong07@gmail.com>
2015-11-17 19:29:04 -0500

    I modify the util_ip_to_hostname util_hostname_to_ip according to the subject talked by Professor on Monday's class

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 * @File: utils.c
 * @Date: 2015-11-10 22:56:21
 * @Last Modified by:   Yinlong Su
-* @Last Modified time: 2015-11-14 20:32:15
+* @Last Modified time: 2015-11-17 19:24:33
 * @Description:
 *     Util function library, some miscellaneous helper functions
 *     + void util_ip_to_hostname(const char *ipaddr, char *hostname)
@@ -20,36 +20,27 @@
  *
  *  @param  : const char    *ipaddr     [IP address]
  *            char          *hostname   [Hostname]
- *  @return : void
+ *  @return : int           [ -1 if failed ]
  *
  *  Convert IP address to hostname
  * --------------------------------------------------------------------------
  */
-void util_ip_to_hostname(const char *ipaddr, char *hostname) {
-    if (strcmp(ipaddr, "130.245.156.19") == 0)
-        strcpy(hostname, "minix");
-    else if (strcmp(ipaddr, "130.245.156.20") == 0)
-        strcpy(hostname, "vm10");
-    else if (strcmp(ipaddr, "130.245.156.21") == 0)
-        strcpy(hostname, "vm1");
-    else if (strcmp(ipaddr, "130.245.156.22") == 0)
-        strcpy(hostname, "vm2");
-    else if (strcmp(ipaddr, "130.245.156.23") == 0)
-        strcpy(hostname, "vm3");
-    else if (strcmp(ipaddr, "130.245.156.24") == 0)
-        strcpy(hostname, "vm4");
-    else if (strcmp(ipaddr, "130.245.156.25") == 0)
-        strcpy(hostname, "vm5");
-    else if (strcmp(ipaddr, "130.245.156.26") == 0)
-        strcpy(hostname, "vm6");
-    else if (strcmp(ipaddr, "130.245.156.27") == 0)
-        strcpy(hostname, "vm7");
-    else if (strcmp(ipaddr, "130.245.156.28") == 0)
-        strcpy(hostname, "vm8");
-    else if (strcmp(ipaddr, "130.245.156.29") == 0)
-        strcpy(hostname, "vm9");
-    else
-        strcpy(hostname, "unknown");
+int util_ip_to_hostname(const char *ipaddr, char *hostname) {
+    struct sockaddr_in  servaddr;
+    struct hostent      *he;
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+
+    inet_pton(AF_INET, ipaddr, &servaddr.sin_addr);
+    he = gethostbyaddr(&servaddr.sin_addr, sizeof(servaddr.sin_addr), AF_INET);
+    if (he == NULL) {
+        printf("util_ip_to_hostname error: gethostbyaddr error for %s\n", ipaddr);
+        return -1;
+    }
+    strncpy(hostname, he->h_name, HOSTNAME_BUFFSIZE);
+
+    return 0;
 }
 
 /* --------------------------------------------------------------------------
@@ -59,30 +50,18 @@ void util_ip_to_hostname(const char *ipaddr, char *hostname) {
  *
  *  @param  : const char    *hostname   [Hostname]
  *            char          *ipaddr     [IP address]
- *  @return : void
+ *  @return : int           [ -1 if failed ]
  *
  *  Convert hostname to IP address
  * --------------------------------------------------------------------------
  */
-void util_hostname_to_ip(const char *hostname, char *ipaddr) {
-    if (strcmp(hostname, "vm10") == 0)
-        strcpy(ipaddr, "130.245.156.20");
-    else if (strcmp(hostname, "vm1") == 0)
-        strcpy(ipaddr, "130.245.156.21");
-    else if (strcmp(hostname, "vm2") == 0)
-        strcpy(ipaddr, "130.245.156.22");
-    else if (strcmp(hostname, "vm3") == 0)
-        strcpy(ipaddr, "130.245.156.23");
-    else if (strcmp(hostname, "vm4") == 0)
-        strcpy(ipaddr, "130.245.156.24");
-    else if (strcmp(hostname, "vm5") == 0)
-        strcpy(ipaddr, "130.245.156.25");
-    else if (strcmp(hostname, "vm6") == 0)
-        strcpy(ipaddr, "130.245.156.26");
-    else if (strcmp(hostname, "vm7") == 0)
-        strcpy(ipaddr, "130.245.156.27");
-    else if (strcmp(hostname, "vm8") == 0)
-        strcpy(ipaddr, "130.245.156.28");
-    else if (strcmp(hostname, "vm9") == 0)
-        strcpy(ipaddr, "130.245.156.29");
+int util_hostname_to_ip(const char *hostname, char *ipaddr) {
+    struct hostent      *he;
+    he = gethostbyname(hostname);
+    if (he == NULL) {
+        printf("util_hostname_to_ip error: gethostbyname error for %s\n", hostname);
+        return -1;
+    }
+    strncpy(ipaddr, inet_ntoa(*(struct in_addr*)he->h_addr), IPADDR_BUFFSIZE);
+    return 0;
 }
